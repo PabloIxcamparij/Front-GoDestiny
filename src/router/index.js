ddb40d7f0a33c8db570d24169d380d2eb9967bc3
jose vargas <jose.vargaslopez@ucr.ac.cr>
2024-05-31 00:47:30 -0600

    peru

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import {createRouter, createWebHistory}from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 //hola causas
-
+//peru
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
