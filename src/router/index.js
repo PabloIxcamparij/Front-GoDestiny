3b0916ebcbf754a8f53b18bd62823c545028728c
jose vargas <jose.vargaslopez@ucr.ac.cr>
2024-05-31 00:46:05 -0600

    hola causas

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
 import {createRouter, createWebHistory}from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-
+//hola causas
 
 
 const router = createRouter({
